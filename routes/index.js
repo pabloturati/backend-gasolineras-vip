1b7f1c97259d53017230ac3a726f84918bc899fa
pabloturati <pablo.turati@gmail.com>
2018-08-07 13:27:59 -0500

    first deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  // res.render('index');
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
+
+
 module.exports = router;
+
